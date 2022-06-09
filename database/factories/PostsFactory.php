985eb1ca203a2a4b57ae3cc187fecc9cb75f75e8
Mahdi <mahdibarati696@gmail.com>
2022-06-09 20:16:12 +0430

    start Project

---
+++ database/factories/PostsFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Posts;
+use Faker\Generator as Faker;
+
+$factory->define(Posts::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
