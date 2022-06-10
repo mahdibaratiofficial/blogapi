e97c6f3fd8eee84d740576a87b0f277e0129a8fb
Mahdi <mahdibarati696@gmail.com>
2022-06-10 12:45:07 +0430

    Add Require Routes

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +16,47 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+/**
+ * Documentation : How To Use Api
+ */
+Route::get('docs', 'HomeController@index')->name('documentation');
+
+/**
+ * Posts Routes
+ */
+Route::get('posts', "PostsController@allPosts");
+Route::get('post/{IdOrSlugPost}', 'PostsController@GetPost');
+
+
+/**
+ * Comments Routes
+ */
+Route::get("comments", "CommentsController@AllComments");
+Route::get('comment/{IdOrSlugComment}', "CommentsController@GetComment");
+
+/**
+ * Users Routes
+ */
+Route::get('users', "UsersController@GetAllUser");
+Route::get('@{UserNameOrUserEmail}', "UsersController@GetUser");
+Route::get("@{UserNameOrUserEmail}/posts", "UserController@GetUserPosts");
+Route::get("postinfo", "UsersController@PostInfo");
+
+
+/**
+ * Categories Routes
+ */
+Route::get('categories',"CategoryController@AllCategory");
+Route::get('{PostSlugOrPostId}/categories',"CategoryController@PostCategory");
+Route::get('posts/{CategoryidOrCategoryName}',"CategoryController@CateGoriesOfEachPost");
+
+
+/**
+ * 
+ * Faker Routes
+ */
+Route::get('faker/post',"PostsController@PostFaker");
+Route::get('faker/comment',"CommentsController@CommentFaker");
+Route::get('faker/user',"UsersController@UserFaker");
+Route::get('faker/category',"CategoryController@CategoryFaker");
+
