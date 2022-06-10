2ced1dc637f4af21f89956c66c629989ff8b1506
Mahdi <mahdibarati696@gmail.com>
2022-06-10 13:22:18 +0430

    Use Sluggable on Model

--- app/Posts.php
+++ app/Posts.php
@@ -1,10 +1,30 @@
 <?php
 
 namespace App;
+use Cviebrock\EloquentSluggable\Sluggable;
 
 use Illuminate\Database\Eloquent\Model;
 
 class Posts extends Model
 {
-    //
+
+    use Sluggable;
+    protected $table = 'posts';
+
+    protected $fillable = [
+        'title',
+        'body',
+        'slug',
+        'author'
+    ];
+
+
+    public function sluggable(): array
+    {
+        return [
+            'slug' => [
+                'source' => 'title'
+            ]
+        ];
+    }
 }
