0122ccc716b2134026017cbe3af36ca6c94a0a3f
Mahdi <mahdibarati696@gmail.com>
2022-06-10 15:59:32 +0430

    dont change i just commited for clean my untracked file

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Posts;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Route;
 
@@ -31,7 +32,6 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         //
-
         parent::boot();
     }
 
