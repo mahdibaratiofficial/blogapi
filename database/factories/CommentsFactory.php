7d49b1df8ff734f72642e2636a4ce6b9691b228c
Mahdi <mahdibarati696@gmail.com>
2022-06-12 14:44:58 +0430

    Chenge Comment Factory file

--- database/factories/CommentsFactory.php
+++ database/factories/CommentsFactory.php
@@ -7,8 +7,6 @@ use Faker\Generator as Faker;
 
 $factory->define(Comments::class, function (Faker $faker) {
     return [
-        'email'=>$faker->safeEmail(),
-        'name'=>$faker->lastName(),
         'posts_id'=>rand(1,4),
         'body'=>$faker->realText(100)
     ];
